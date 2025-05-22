a7da2b261dbbc4a07a43dc7535be370d06a4be43
Diya Bhargava <117004051+diyabhargava27@users.noreply.github.com>
2025-05-22 20:32:39 +0530

    Update Login.jsx

--- frontend/budgit-fit/src/pages/Auth/Login.jsx
+++ frontend/budgit-fit/src/pages/Auth/Login.jsx
@@ -6,7 +6,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
-import { UserContext } from '../../context/userContext';
+import { UserContext } from '../../context/UserContext';
 
 const Login = () => {
   const [email, setEmail] = useState('');
